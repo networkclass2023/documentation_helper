1706d48508c40ff664999647ac28169fb008a9b1
m.khaleghi <mortezaprogram@gmail.com>
2024-02-15 17:57:06 +0330

    streamlit

--- backend/core.py
+++ backend/core.py
@@ -14,21 +14,43 @@ from consts import INDEX_NAME
 
 pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
+import pinecone
+   
+    
+import os
+from typing import Any, List, Dict
 
-def run_llm(query: str) -> Any:
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.chat_models import ChatOpenAI
+# from langchain_community.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+# from langchain_pinecone import Pinecone
+# import pinecone
+# PINECONE_API_KEY="df229a77-4e4f-4c31-8a39-ceba51e11858"
+
+from consts import INDEX_NAME
+
+# pinecone.init(
+    # api_key=os.environ["PINECONE_API_KEY"],
+#    
+# )
+# print(PINECONE_API_KEY)
+# pc=pinecone.init(pinecone_api_key=PINECONE_API_KEY)
+
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = PineconeLangChain.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+
+    return qa.invoke({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
-    print(run_llm(query="What is LangChain?"))
+    print(run_llm(query="What is LangChain?"))    
